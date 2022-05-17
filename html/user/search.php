b9322b14feb1a82fa259685750be5e0484e3235c
Zahjen <zahjen.dev@gmail.com>
2022-05-17 21:06:54 +0200

    Add files via upload

---
+++ html/user/search.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Poppins font family -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+
+    <!-- Material Icon -->
+    <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+    <!-- Styles sheet -->
+    <link rel="stylesheet" media="screen and (min-width: 950px)" href="../../import/import-search/import.css">
+    <link rel="stylesheet" media="screen and (max-width: 949px)" href="../../import/import-search/import-tablet.css">
+    
+    <title>Home</title>
+</head>
+<body>
+
+    <nav class="nav"> 
+        <!-- Burger menu relate to mobile version -->
+        <input type="checkbox" id="nav-checkbox">
+        <label for="nav-checkbox" class="nav-button">
+            <span class="line-1"></span>
+            <span class="line-2"></span>
+            <span class="line-3"></span>
+        </label>
+
+        <!-- Nav bar -->
+        <div class="nav-bar-container">
+            <a href="" class="logo">
+                <img src="../../asset/image/logo.svg" alt="logo">
+            </a>        
+
+            <!-- Navigation link -->
+            <div class="nav-links">
+                <a href="home.php" class="part-link-page">
+                    Home
+                </a>
+                <a href="search.php" class="part-link-page active">
+                    Search
+                </a>
+                <a href="category.php" class="part-link-page">
+                    Category
+                </a>
+                <a href="download.php" class="part-link-page">
+                    Download
+                </a>
+                <a href="profile.php" class="part-link-page">
+                    Profile
+                </a>
+
+                <a href="../../../controller/page/logout.php" class="log-out">
+                    <span class="material-symbols-outlined">
+                        logout
+                    </span>
+                    <span class="log-out-text">
+                        Logout
+                    </span>
+                </a>
+            </div>
+
+            <div class="ghost"></div>
+        </div> 
+    </nav>     
+
+    <section class="section-global">
+
+        <article>
+            <div id="search-container">
+                <img src="../../asset/image/logo-min.svg" alt="logo-min" id="search-img">
+                <div id="search-text">
+                    <span class="search-word">Looking for</span> 
+                    <span class="search-word">something new ?</span>
+                </div>
+            </div>
+
+            <div class="search-bar">
+                <input type="text" name="search-bar" id="search-bar-input">
+                <button type='submit' id="search-bar-btn">
+                    <span class="material-symbols-outlined search-icon">
+                        search
+                    </span>
+                </button>
+            </div>
+
+            <div class="result-container">
+                
+            </div>
+        </article>
+        
+    </section>
+
+    <script src="../../javascript/ajax/book.js"></script>
+</body>
+</html>
